fa28df19b7f0c0e2255e7bab1ae2fa6d782b8e70
Tomás <ertomy@gmail.com>
2017-04-11 20:42:09 +0200

    olvidado subir en la carga inicial

---
+++ resources/lang/es/redessociales.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Rutas de las redes sociales
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'facebook' => 'http://www.facebook.com',
+    'twitter' => 'http://www.twitter.com',
+    'google' => 'http://www.google.com',
+    'youtube' => 'http://www.youtube.com',
+    'vimeo' => 'http://www.vimeo.com',
+    'pinterest' => 'http://www.pinterest.com'
+];
